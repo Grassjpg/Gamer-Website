7018095ffe1b675ab65885f20e9bd7e09c09208b
A Gamer <zhanse0746@greenvilleschools.us>
2019-12-04 16:41:08 +0000

    Initial commit

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+document.getElementById('date').innerHTML = new Date().toDateString();
+
+
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
